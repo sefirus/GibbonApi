464a5c31454491c4cc5d9979d6949fc013335d23
sefirus <sashasam348@gmail.com>
2023-05-30 21:55:53 +0300

    Added DA layer, DbContext

---
+++ DataAccess/GibbonDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+public class GibbonDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Workspace> Workspaces { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<WorkspacePermission> WorkspacePermissions { get; set; }
+    public DbSet<DataType> DataTypes { get; set; }
+    public DbSet<SchemaObject> SchemaObjects { get; set; }
+    public DbSet<SchemaField> SchemaFields { get; set; }
+    public DbSet<FieldValue> FieldValues { get; set; }
+    public DbSet<StoredDocument> StoredDocuments { get; set; }
+}
