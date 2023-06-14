082396a97ca3c55e911c211313c5500b7ec4ecd8
sefirus <sashasam348@gmail.com>
2023-06-15 00:58:41 +0300

    Found approach to get object definition from the user

---
+++ Core/ViewModels/Schema/SchemaFieldViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.ViewModels.Schema;
+
+public class SchemaFieldViewModel
+{
+    public string Type { get; set; }
+    public int Length { get; set; }
+    public bool IsPrimaryKey { get; set; }
+}
---
+++ WebApi/Controllers/SchemaController.cs
@@ -0,0 +1,17 @@
+﻿using Core.ViewModels.Schema;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Helpers;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/schema")]
+public class SchemaController : ControllerBase
+{
+    [HttpPost("create-object")]
+    public async Task<IActionResult> CreateSchemaObject(Dictionary<string, SchemaFieldViewModel> y)
+    {
+        var mem = new Memory<byte>(new byte[HttpContext.Request.ContentLength.Value]);
+        return Ok();
+    }
+}
