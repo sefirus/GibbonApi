49ac0b6cdd1d17adbc902fe0d9aa24fe918d5ab0
sefirus <sashasam348@gmail.com>
2023-07-09 13:06:50 +0300

    Added another testcase for the arrays

--- ApplicationTests/Fixtures/SchemaFieldMappingFixture.cs
+++ ApplicationTests/Fixtures/SchemaFieldMappingFixture.cs
@@ -6,71 +6,120 @@ namespace ApplicationTests.Fixtures;
 
 public class SchemaFieldMappingFixture
 {
-    public Dictionary<string, SchemaFieldViewModel> PrimitiveTypesSource { get; }
-    public List<SchemaField> PrimitiveTypesExpected { get; }
-    public Dictionary<string, SchemaFieldViewModel> MixedTypesWithNestedTypesSource { get; }
-    public List<SchemaField> MixedTypesWithNestedTypesExpected { get; }
-
-    public SchemaFieldMappingFixture()
+    public Dictionary<string, SchemaFieldViewModel> PrimitiveTypesSource => new()
     {
-        // Test case 1 source
-        PrimitiveTypesSource = new Dictionary<string, SchemaFieldViewModel>
-        {
-            { "IntField", new SchemaFieldViewModel { Type = DataTypesEnum.Int, Min = 1, Max = 10 } },
-            { "FloatField", new SchemaFieldViewModel { Type = DataTypesEnum.Float, Min = 1.0f, Max = 5.5f } },
-            { "StringField", new SchemaFieldViewModel { Type = DataTypesEnum.String, Length = 50, Pattern = "^[a-z]*$" } },
-            { "ObjectIdField", new SchemaFieldViewModel { Type = DataTypesEnum.ObjectId } },
-            { "UuidField", new SchemaFieldViewModel { Type = DataTypesEnum.Uuid } },
-        };
+        { "IntField", new SchemaFieldViewModel { Type = DataTypesEnum.Int, Min = 1, Max = 10 } },
+        { "FloatField", new SchemaFieldViewModel { Type = DataTypesEnum.Float, Min = 1.0f, Max = 5.5f } },
+        { "StringField", new SchemaFieldViewModel { Type = DataTypesEnum.String, Length = 50, Pattern = "^[a-z]*$" } },
+        { "ObjectIdField", new SchemaFieldViewModel { Type = DataTypesEnum.ObjectId } },
+        { "UuidField", new SchemaFieldViewModel { Type = DataTypesEnum.Uuid } },
+    };
 
-        // Test case 1 expected
-        PrimitiveTypesExpected = new List<SchemaField>
-        {
-            new SchemaField { FieldName = "IntField", DataTypeId = DataTypesEnum.GetDataTypeObject("Int").Id, Min = 1, Max = 10 },
-            new SchemaField { FieldName = "FloatField", DataTypeId = DataTypesEnum.GetDataTypeObject("Float").Id, Min = 1.0f, Max = 5.5f },
-            new SchemaField { FieldName = "StringField", DataTypeId = DataTypesEnum.GetDataTypeObject("String").Id, Length = 50, Pattern = "^[a-z]*$" },
-            new SchemaField { FieldName = "ObjectIdField", DataTypeId = DataTypesEnum.GetDataTypeObject("ObjectId").Id },
-            new SchemaField { FieldName = "UuidField", DataTypeId = DataTypesEnum.GetDataTypeObject("Uuid").Id },
-        };
-        
-        MixedTypesWithNestedTypesSource = new Dictionary<string, SchemaFieldViewModel>
-        {
-            { "IntField", new SchemaFieldViewModel { Type = DataTypesEnum.Int, Min = -50, Max = 50 } },
-            { "FloatField", new SchemaFieldViewModel { Type = DataTypesEnum.Float, Min = -50.0f, Max = 50.0f } },
-            { "StringField", new SchemaFieldViewModel { Type = DataTypesEnum.String, Length = 20 } },
-            { "ObjectIdField", new SchemaFieldViewModel { Type = DataTypesEnum.ObjectId } },
-            { "UuidField", new SchemaFieldViewModel { Type = DataTypesEnum.Uuid } },
-            { "StringArrayField", new SchemaFieldViewModel { Type = DataTypesEnum.Array, ArrayElement = new SchemaFieldViewModel { Type = DataTypesEnum.String, Length = 15 } } },
-            { "IntArrayField", new SchemaFieldViewModel { Type = DataTypesEnum.Array, ArrayElement = new SchemaFieldViewModel { Type = DataTypesEnum.Int, Min = 0, Max = 100 } } },
-        };
+    public List<SchemaField> PrimitiveTypesExpected { get; } = new()
+    {
+        new SchemaField { FieldName = "IntField", DataTypeId = DataTypesEnum.GetDataTypeObject("Int").Id, Min = 1, Max = 10 },
+        new SchemaField { FieldName = "FloatField", DataTypeId = DataTypesEnum.GetDataTypeObject("Float").Id, Min = 1.0f, Max = 5.5f },
+        new SchemaField { FieldName = "StringField", DataTypeId = DataTypesEnum.GetDataTypeObject("String").Id, Length = 50, Pattern = "^[a-z]*$" },
+        new SchemaField { FieldName = "ObjectIdField", DataTypeId = DataTypesEnum.GetDataTypeObject("ObjectId").Id },
+        new SchemaField { FieldName = "UuidField", DataTypeId = DataTypesEnum.GetDataTypeObject("Uuid").Id },
+    };
 
-        // Test case 2 expected
-        MixedTypesWithNestedTypesExpected = new List<SchemaField>
+    public Dictionary<string, SchemaFieldViewModel> MixedTypesWithNestedTypesSource => new()
+    {
+        { "IntField", new SchemaFieldViewModel { Type = DataTypesEnum.Int, Min = -50, Max = 50 } },
+        { "FloatField", new SchemaFieldViewModel { Type = DataTypesEnum.Float, Min = -50.0f, Max = 50.0f } },
+        { "StringField", new SchemaFieldViewModel { Type = DataTypesEnum.String, Length = 20 } },
+        { "ObjectIdField", new SchemaFieldViewModel { Type = DataTypesEnum.ObjectId } },
+        { "UuidField", new SchemaFieldViewModel { Type = DataTypesEnum.Uuid } },
+        { "StringArrayField", new SchemaFieldViewModel { Type = DataTypesEnum.Array, ArrayElement = new SchemaFieldViewModel { Type = DataTypesEnum.String, Length = 15 } } },
+        { "IntArrayField", new SchemaFieldViewModel { Type = DataTypesEnum.Array, ArrayElement = new SchemaFieldViewModel { Type = DataTypesEnum.Int, Min = 0, Max = 100 } } },
+    };
+
+    public List<SchemaField> MixedTypesWithNestedTypesExpected => new()
+    {
+        new () { FieldName = "IntField", DataTypeId = DataTypesEnum.GetDataTypeObject("Int").Id, Min = -50, Max = 50 },
+        new () { FieldName = "FloatField", DataTypeId = DataTypesEnum.GetDataTypeObject("Float").Id, Min = -50.0f, Max = 50.0f },
+        new () { FieldName = "StringField", DataTypeId = DataTypesEnum.GetDataTypeObject("String").Id, Length = 20 },
+        new () { FieldName = "ObjectIdField", DataTypeId = DataTypesEnum.GetDataTypeObject("ObjectId").Id },
+        new () { FieldName = "UuidField", DataTypeId = DataTypesEnum.GetDataTypeObject("Uuid").Id },
+        new ()
         {
-            new () { FieldName = "IntField", DataTypeId = DataTypesEnum.GetDataTypeObject("Int").Id, Min = -50, Max = 50 },
-            new () { FieldName = "FloatField", DataTypeId = DataTypesEnum.GetDataTypeObject("Float").Id, Min = -50.0f, Max = 50.0f },
-            new () { FieldName = "StringField", DataTypeId = DataTypesEnum.GetDataTypeObject("String").Id, Length = 20 },
-            new () { FieldName = "ObjectIdField", DataTypeId = DataTypesEnum.GetDataTypeObject("ObjectId").Id },
-            new () { FieldName = "UuidField", DataTypeId = DataTypesEnum.GetDataTypeObject("Uuid").Id },
-            new ()
+            FieldName = "StringArrayField", 
+            DataTypeId = DataTypesEnum.GetArrayDataTypeObject("String").Id, 
+            IsArray = true, 
+            ChildFields = new()
             {
-                FieldName = "StringArrayField", 
-                DataTypeId = DataTypesEnum.GetArrayDataTypeObject("String").Id, 
-                IsArray = true, 
-                ChildFields = new()
-                {
-                    new (){ DataTypeId = DataTypesEnum.GetDataTypeObject("String").Id, Length = 15 }
-                },
+                new (){ DataTypeId = DataTypesEnum.GetDataTypeObject("String").Id, Length = 15 }
             },
-            new () { 
-                FieldName = "IntArrayField", 
-                DataTypeId = DataTypesEnum.GetArrayDataTypeObject("Int").Id, 
-                IsArray = true, 
-                ChildFields = new()
-                {
-                    new (){ DataTypeId = DataTypesEnum.GetDataTypeObject("Int").Id, Min = 0, Max = 100}
-                } 
+        },
+        new () { 
+            FieldName = "IntArrayField", 
+            DataTypeId = DataTypesEnum.GetArrayDataTypeObject("Int").Id, 
+            IsArray = true, 
+            ChildFields = new()
+            {
+                new (){ DataTypeId = DataTypesEnum.GetDataTypeObject("Int").Id, Min = 0, Max = 100}
+            } 
+        },
+    };
+
+    public Dictionary<string, SchemaFieldViewModel> ArrayOfPrimitivesSource => new()
+    {
+        { "IntArrayField", new SchemaFieldViewModel { Type = DataTypesEnum.Array, ArrayElement = new SchemaFieldViewModel { Type = DataTypesEnum.Int, Min = -10, Max = 10 } } },
+        { "FloatArrayField", new SchemaFieldViewModel { Type = DataTypesEnum.Array, ArrayElement = new SchemaFieldViewModel { Type = DataTypesEnum.Float, Min = -10.0f, Max = 10.0f } } },
+        { "StringArrayField", new SchemaFieldViewModel { Type = DataTypesEnum.Array, ArrayElement = new SchemaFieldViewModel { Type = DataTypesEnum.String, Length = 20 } } },
+        { "ObjectIdArrayField", new SchemaFieldViewModel { Type = DataTypesEnum.Array, ArrayElement = new SchemaFieldViewModel { Type = DataTypesEnum.ObjectId } } },
+        { "UuidArrayField", new SchemaFieldViewModel { Type = DataTypesEnum.Array, ArrayElement = new SchemaFieldViewModel { Type = DataTypesEnum.Uuid } } }
+    };
+
+    public List<SchemaField> ArrayOfPrimitivesExpected => new()
+    {
+        new () { 
+            FieldName = "IntArrayField", 
+            DataTypeId = DataTypesEnum.GetArrayDataTypeObject("Int").Id, 
+            IsArray = true, 
+            ChildFields = new()
+            {
+                new SchemaField(){ FieldName = "IntArrayField", DataTypeId = DataTypesEnum.GetDataTypeObject("Int").Id, Min = -10, Max = 10}
+            } 
+        },
+        new () { 
+            FieldName = "FloatArrayField", 
+            DataTypeId = DataTypesEnum.GetArrayDataTypeObject("Float").Id, 
+            IsArray = true, 
+            ChildFields = new()
+            {
+                new (){ FieldName = "FloatArrayField", DataTypeId = DataTypesEnum.GetDataTypeObject("Float").Id, Min = -10.0f, Max = 10.0f}
+            } 
+        },
+        new ()
+        {
+            FieldName = "StringArrayField", 
+            DataTypeId = DataTypesEnum.GetArrayDataTypeObject("String").Id, 
+            IsArray = true, 
+            ChildFields = new()
+            {
+                new SchemaField(){ FieldName = "StringArrayField", DataTypeId = DataTypesEnum.GetDataTypeObject("String").Id, Length = 20 }
             },
-        };
-    }
+        },
+        new ()
+        {
+            FieldName = "ObjectIdArrayField", 
+            DataTypeId = DataTypesEnum.GetArrayDataTypeObject("ObjectId").Id, 
+            IsArray = true, 
+            ChildFields = new()
+            {
+                new (){ FieldName = "ObjectIdArrayField", DataTypeId = DataTypesEnum.GetDataTypeObject("ObjectId").Id }
+            },
+        },
+        new () { 
+            FieldName = "UuidArrayField", 
+            DataTypeId = DataTypesEnum.GetArrayDataTypeObject("Uuid").Id, 
+            IsArray = true, 
+            ChildFields = new()
+            {
+                new (){ FieldName = "UuidArrayField", DataTypeId = DataTypesEnum.GetDataTypeObject("Uuid").Id }
+            } 
+        }
+    };
 }
--- ApplicationTests/SchemaObjectMapperTests.cs
+++ ApplicationTests/SchemaObjectMapperTests.cs
@@ -51,7 +51,7 @@ public class SchemaObjectMapperTests : IClassFixture<SchemaFieldMappingFixture>
             Assert.Equal(expectedField.DataTypeId, resultField.DataTypeId);
             Assert.Equal(expectedField.IsArray, resultField.IsArray);
         
-            if (expected[i].ChildFields != null && expected[i].IsArray)
+            if (expectedField is { ChildFields: not null, IsArray: true })
             {
                 var expectedChildField = expectedField.ChildFields.Single();
                 var resultChildField = resultField.ChildFields.Single();
@@ -72,5 +72,49 @@ public class SchemaObjectMapperTests : IClassFixture<SchemaFieldMappingFixture>
             }
         }
     }
+    
+    [Fact]
+    public void Map_ShouldCorrectlyMapArrayOfPrimitivesTestCase()
+    {
+        // Arrange
+        var source = _fixture.ArrayOfPrimitivesSource;
+        var expected = _fixture.ArrayOfPrimitivesExpected;
+
+        // Act
+        var result = _mapper.Map(source);
+
+        // Assert
+        Assert.Equal(expected.Count, result.Count);
+
+        for (var i = 0; i < result.Count; i++)
+        {
+            var expectedField = expected[i];
+            var resultField = result[i];
+            Assert.Equal(expectedField.FieldName, resultField.FieldName);
+            Assert.Equal(expectedField.DataTypeId, resultField.DataTypeId);
+            Assert.Equal(expectedField.IsArray, resultField.IsArray);
+        
+            if (expectedField is { ChildFields: not null, IsArray: true })
+            {
+                var expectedChildField = expectedField.ChildFields.Single();
+                var resultChildField = resultField.ChildFields.Single();
+                Assert.Single(resultField.ChildFields);
+                Assert.Equal(expectedField.FieldName, resultChildField.FieldName);
+                Assert.Equal(expectedChildField.DataTypeId, resultChildField.DataTypeId);
+                Assert.Equal(expectedChildField.Min, resultChildField.Min);
+                Assert.Equal(expectedChildField.Max, resultChildField.Max);
+                Assert.Equal(expectedChildField.Pattern, resultChildField.Pattern);
+                Assert.Equal(expectedChildField.Length, resultChildField.Length);
+            }
+            else
+            {
+                Assert.Equal(expectedField.Min, resultField.Min);
+                Assert.Equal(expectedField.Max, resultField.Max);
+                Assert.Equal(expectedField.Pattern, resultField.Pattern);
+                Assert.Equal(expectedField.Length, resultField.Length);
+            }
+        }
+    }
+
 
 }
