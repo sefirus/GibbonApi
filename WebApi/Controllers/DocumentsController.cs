9c92fcf76f5f1410542e55a9b7d6287d920c88f4
sefirus <sashasam348@gmail.com>
2023-07-15 13:57:13 +0300

    Removed excessive columns, added controller for documents

---
+++ WebApi/Controllers/DocumentsController.cs
@@ -0,0 +1,17 @@
+﻿using Core.Enums;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/")]
+public class DocumentsController : ControllerBase
+{
+    [Authorize(Roles = AccessLevels.GeneralAccess)]
+    [HttpPost("{workspaceId:guid}/{objectName}")]
+    public async Task PostObject(string objectName, Guid workspaceId)
+    {
+        var x = HttpContext.Request.Body;
+    }
+}
